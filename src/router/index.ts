673815efdc84482de8a591bca12d3cfa3f45a364
i4nizer <iansandoval264@gmail.com>
2026-01-26 17:02:47 +0800

    Created welcome page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import WelcomeView from '@/views/WelcomeView.vue'
+
+//
 
 const routes = [
     {
         path: '/',
-        name: 'home',
-        component: HomeView,
+        name: 'welcome',
+        component: WelcomeView,
     },
 ]
 
@@ -13,5 +15,7 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 })
+
+//
 
 export default router
