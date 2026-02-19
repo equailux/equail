a84c7162263a558064bc693f60529ed8272d4813
equailux <equail2026@gmail.com>
2026-02-20 02:00:19 +0800

    Overhauled the controls page design.

--- src/views/app/ControlsView.vue
+++ src/views/app/ControlsView.vue
@@ -1,62 +1,77 @@
 <template>
-	<v-container class="pa-0" fluid>
+	<v-container class="bg-secondary" fluid>
+		<v-row dense>
+			<v-col cols="12" sm="8">
+				<div class="pb-4">
+					<small class="text-accent">Manage you devices</small>
+					<h3>Device Controls</h3>
+				</div>
+			</v-col>
+		</v-row>
 		<v-row dense>
 			<v-col cols="12" sm="6">
-				<v-card color="primary" class="h-100" elevation="0">
-					<template #prepend>
-						<v-avatar color="primary">
-							<v-img eager src="/icon/light-bulb.png"></v-img>
-						</v-avatar>
-					</template>
-					<template #title>
-						<span class="pb-0 text-subtitle-2">Lighting Control</span>
-					</template>
-					<template #subtitle>
-						<span class="pb-0 text-subtitle-2 font-weight-regular">Manage lighting</span>
-					</template>
-					<template #text>
-						<div class="d-flex align-center justify-space-between">
-							<div>
-								<span class="text-subtitle-2">Lighting Status</span>
-								<br />
-								<span class="text-grey-darken-2 text-subtitle-2 font-weight-regular">Turn On/Off</span>
-							</div>
-							<v-switch
-								inset
-								hide-details
-								color="primary"
-								density="compact"
-								v-model="isLightOn"
-							></v-switch>
+				<div class="pa-4 rounded-lg bg-primary border elevation-1 d-flex flex-column">
+					<div class="d-flex align-center ga-3">
+						<div 
+							class="pa-4 rounded-lg d-flex align-center justify-center"
+							style="width: 38px; height: 38px; background-color: rgba(var(--v-theme-accent), 0.1)"
+						>
+							<v-icon color="accent">mdi-waves</v-icon>
 						</div>
-					</template>
-				</v-card>
+						<div class="w-100">
+							<div class="d-flex align-center justify-space-between">
+								<h5>Water Pump</h5>
+								<div 
+									class="text-caption text-accent px-3 rounded-pill"
+									style="background-color: rgba(var(--v-theme-accent), 0.1)"
+								>Off</div>
+							</div>
+							<small 
+								class="text-grey-darken-1 text-caption"
+								style="line-height: 0;"
+							>Automated water distribution</small>
+						</div>
+					</div>
+					<v-btn
+						text="Turn On"
+						color="accent"
+						class="mt-4"
+						prepend-icon="mdi-power"
+					></v-btn>
+					<span class="mt-3 text-grey-darken-1 text-caption">Idle - Standby mode</span>
+				</div>
 			</v-col>
 			<v-col cols="12" sm="6">
-				<v-card color="primary" elevation="0">
-					<v-card-title class="text-subtitle-2">About Lighting Control</v-card-title>
-					<v-card-text>
-						<span>Purpose of light:</span>
-						<br />
-						<span>&nbsp;&nbsp;&nbsp;&nbsp;Helps quails see food and water.</span>
-						<br />
-						<br />
-						<ul class="px-5">
-							<li>During feeding and drinking times.</li>
-							<li>8-10 hours of light per day.</li>
-							<li>Dim or warm/red light is preferred to reduce stress.</li>
-							<li>Keep the rest of the day dark to allow recovery and minimize movement.</li>
-						</ul>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col cols="12" sm="6">
-				<v-card color="primary" elevation="0">
-					<v-card-title class="text-subtitle-2">Mortality Records</v-card-title>
-					<v-card-text>
-						<MortalityCreateForm @submit="onSubmitMortalityCreateForm" />
-					</v-card-text>
-				</v-card>
+				<div class="pa-4 rounded-lg bg-primary border elevation-1 d-flex flex-column">
+					<div class="d-flex align-center ga-3">
+						<div 
+							class="pa-4 rounded-lg d-flex align-center justify-center"
+							style="width: 38px; height: 38px; background-color: rgba(var(--v-theme-accent), 0.1)"
+						>
+							<v-icon color="accent">mdi-lightbulb-outline</v-icon>
+						</div>
+						<div class="w-100">
+							<div class="d-flex align-center justify-space-between">
+								<h5>Coop Light</h5>
+								<div 
+									class="text-caption text-accent px-3 rounded-pill"
+									style="background-color: rgba(var(--v-theme-accent), 0.1)"
+								>Off</div>
+							</div>
+							<small 
+								class="text-grey-darken-1 text-caption"
+								style="line-height: 0;"
+							>LED lighting control</small>
+						</div>
+					</div>
+					<v-btn
+						text="Turn On"
+						color="accent"
+						class="mt-4"
+						prepend-icon="mdi-power"
+					></v-btn>
+					<span class="mt-3 text-grey-darken-1 text-caption">Idle - Standby mode</span>
+				</div>
 			</v-col>
 		</v-row>
 	</v-container>
