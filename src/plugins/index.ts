6557eeb8829975e6edf27b3e4e3816b0ece18a7a
equailux <equail2026@gmail.com>
2026-03-17 08:29:22 +0800

    Added axios instance api plugin.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,7 @@
 import type { App } from "vue"
 import vuetify from "./vuetify"
 import pinia from "./pinia"
+import api from "./api"
 import "chart.js/auto"
 
 //
@@ -8,6 +9,7 @@ import "chart.js/auto"
 const registerPlugins = (app: App) => {
 	app.use(vuetify)
 	app.use(pinia)
+	app.use(api)
 }
 
 //
