8f008fd929570f7ce1f11d35154a0feece7ad0b7
equailux <equail2026@gmail.com>
2026-03-19 00:27:55 +0800

    Implemented real reading and egg data in analytics page.

--- src/views/app/AnalyticsView.vue
+++ src/views/app/AnalyticsView.vue
@@ -26,7 +26,7 @@
 					<small class="text-grey text-caption">Average monthly temperature in celcius</small>
 					<ReadingLineChart
 						:color="theme.current.value.colors.accent"
-						:readings
+						:readings="temperatureTVPair"
 					></ReadingLineChart>
 				</div>
 			</v-col>
@@ -36,7 +36,7 @@
 					<small class="text-grey text-caption">Average monthly humidity percentage</small>
 					<ReadingLineChart
 						:color="theme.current.value.colors.accent"
-						:readings
+						:readings="humidityTVPair"
 					></ReadingLineChart>
 				</div>
 			</v-col>
@@ -46,7 +46,7 @@
 					<small class="text-grey text-caption">Total eggs collected per month</small>
 					<ReadingBarChart
 						:color="theme.current.value.colors.accent"
-						:readings
+						:readings="captureDetectionsPerMonth"
 					></ReadingBarChart>
 				</div>
 			</v-col>
@@ -90,9 +90,15 @@
 import ReadingBarChart from '@/components/app/analytics/ReadingBarChart.vue';
 import ReadingLineChart from '@/components/app/analytics/ReadingLineChart.vue';
 import useReportExport from '@/composables/use-report-export';
+import type { CaptureSchema } from '@/schemas/CaptureSchema';
 import type { MortalitySchema } from '@/schemas/MortalitySchema';
+import type { ReadingSchema } from '@/schemas/ReadingSchema';
+import { useCaptureStore } from '@/stores/capture';
+import { useDetectionStore } from '@/stores/detection';
 import { useMortalityStore } from '@/stores/mortality';
+import { useReadingStore } from '@/stores/reading';
 import { useToastStore } from '@/stores/toast';
+import { groupByKey } from '@/utils/group';
 import { Capacitor } from '@capacitor/core';
 import { storeToRefs } from 'pinia';
 import { computed, nextTick, onMounted, reactive, ref } from 'vue';
@@ -103,27 +109,91 @@ import { useDate, useTheme } from 'vuetify';
 // --- Utils
 const theme = useTheme()
 const dateCmp = useDate()
+const isNative = Capacitor.isNativePlatform()
 const toastStore = useToastStore()
 
-// --- Data
-const readings = reactive<Record<string, number>>({
-	January: 8,
-	February: 13,
-	March: 17,
-	April: 20,
-})
+// --- Readings
+const readingStore = useReadingStore()
+const { temperatures, humidities } = storeToRefs(readingStore)
+const humidityTVPair = computed(() => getAverageReadingsPerMonth(humidities.value))
+const temperatureTVPair = computed(() => getAverageReadingsPerMonth(temperatures.value))
 
-// --- Platform
-const isNative = Capacitor.isNativePlatform()
+const groupReadingsByMonth = (readings: ReadingSchema[]) => {
+	const group: Record<string, ReadingSchema[]> = {}
+	for (const r of readings) {
+		const key = dateCmp.format(r.createdAt, "month")
+		if (!(key in group)) group[key] = []
+		group[key]!.push(r)
+	}
+	return group
+}
 
-//
+const averageGroupReadingsByMonth = (readings: Record<string, ReadingSchema[]>) => {
+	const avg = (readings: ReadingSchema[]) => readings.reduce((p, c) => p + c.value, 0) / readings.length
+	const group: Record<string, number> = {}
+	for (const month in readings) group[month] = avg(readings[month]!)
+	return group
+}
+
+const getAverageReadingsPerMonth = (readings: ReadingSchema[]) => {
+	const readingsByMonth = groupReadingsByMonth(readings)
+	return averageGroupReadingsByMonth(readingsByMonth)
+}
+
+// --- Capture
+const captureStore = useCaptureStore()
+const { captures } = storeToRefs(captureStore)
+const capturesByCam = computed(() => groupByKey(captures.value, (c) => c.camera))
+const captureDetectionsPerMonth = computed(() => countCaptureDetectionsPerMonth(capturesByCam.value))
+
+// --- Detections
+const detectionStore = useDetectionStore()
+const { detections } = storeToRefs(detectionStore)
+const detectionsByCid = computed(() => groupByKey(detections.value, (d) => d.captureId))
+
+// --- Egg Summary
+const countCaptureDetectionsPerMonth = (cameraCapturesMap: Map<string, CaptureSchema[]>) => {
+	const group: Record<string, number> = {}
+
+	for (const [camera, captures] of cameraCapturesMap.entries()) {
+		const capturesWithCount = captures.map((c) => ({ ...c, count: countCaptureDetections(c, captures) }))
+		const totalCapturesWithCountByMonth = sumCapturesWithCountByMonth(capturesWithCount)
+
+		for (const month in totalCapturesWithCountByMonth) {
+			if (!(month in group)) group[month] = 0
+			group[month] = group[month]! + totalCapturesWithCountByMonth[month]!
+		}
+	}
+
+	return group
+}
+
+const countCaptureDetections = (capture: CaptureSchema, captures: CaptureSchema[]) => {
+	const index = captures.findIndex(c => c.id == capture.id)
+	if (index == -1 || index - 1 < 0) return detectionsByCid.value.get(capture.id)?.length || 0
+
+	const captureDetections = detectionsByCid.value.get(capture.id)?.length || 0
+	const prevCapture = captures[index - 1]!
+	const prevCaptureDetections = detectionsByCid.value.get(prevCapture.id)?.length || 0
+	return captureDetections - prevCaptureDetections
+}
+
+const sumCapturesWithCountByMonth = (captures: (CaptureSchema & { count: number })[]) => {
+	const group: Record<string, number> = {}
+	for (const capture of captures) {
+		const month = dateCmp.format(capture.createdAt, "month")
+		if (!(month in group)) group[month] = 0
+		group[month] = group[month]! + capture.count
+	}
+	return group
+}
 
 // --- Mortalities
 const mortalityStore = useMortalityStore()
 const { mortalities } = storeToRefs(mortalityStore)
-const mortalitiesByMonth = computed(() => groupByMonth(mortalities.value))
+const mortalitiesByMonth = computed(() => groupMortalityByMonth(mortalities.value))
 
-const groupByMonth = (mortalities: MortalitySchema[]) => {
+const groupMortalityByMonth = (mortalities: MortalitySchema[]) => {
 	const group: Record<string, number> = {}
 	const data = [...mortalities].sort((a, b) => a.date.getTime() - b.date.getTime())
 
@@ -156,7 +226,12 @@ const onClickExportPDF = async () => {
 //
 
 const onMountedCb = async () => {
-	await Promise.all([mortalityStore.retrieve()])
+	await Promise.all([
+		captureStore.retrieve(),
+		detectionStore.retrieve(),
+		readingStore.retrieve(),
+		mortalityStore.retrieve(),
+	])
 }
 
 onMounted(async () => await onMountedCb().catch(toastStore.error))
