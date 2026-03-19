50059bfd2520c09dd581bd4e33d990bfef7c0c70
equailux <equail2026@gmail.com>
2026-03-20 00:33:14 +0800

    Created server store to ping api ahead of time.

---
+++ src/stores/server.ts
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia"
+import { ref } from "vue"
+
+//
+
+export const useServerStore = defineStore("server", () => {
+    
+    //
+
+    const url = ref("")
+    const busy = ref(false)
+    const alive = ref(false)
+    const interval = ref<ReturnType<typeof setInterval>>()
+
+    //
+
+    const connect = async (server: string, heartbeat: number = 15000) => {
+        url.value = server
+        const callback = () => Promise
+            .resolve()
+            .then(() => busy.value = true)
+            .then(() => fetch(server))
+            .then(() => alive.value = true)
+            .catch(() => alive.value = false)
+            .finally(() => busy.value = false)
+        interval.value = setInterval(() => callback(), heartbeat)
+        await callback()
+    }
+
+    const disconnect = () => {
+        busy.value = false
+        alive.value = false
+        clearInterval(interval.value)
+        interval.value = undefined
+    }
+
+    //
+
+    return { url, busy, alive, interval, connect, disconnect }
+})
