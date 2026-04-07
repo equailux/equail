60024058ea92ac1de733ab139974cd445adeccad
equailux <equail2026@gmail.com>
2026-04-08 02:01:37 +0800

    Registered better-auth auth client plugin.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,12 +3,14 @@ import vuetify from "./vuetify"
 import pinia from "./pinia"
 import api from "./api"
 import "chart.js/auto"
+import auth from "./auth"
 
 //
 
 const registerPlugins = (app: App) => {
 	app.use(vuetify)
 	app.use(pinia)
+	app.use(auth)
 	app.use(api)
 }
 
