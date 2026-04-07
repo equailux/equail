dace42fd49ceaded0a867390096e581aaf294d97
equailux <equail2026@gmail.com>
2026-04-08 02:02:33 +0800

    Updated user schema.

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -1,29 +1,21 @@
-import z from "zod";
+import z from "zod"
 
 //
 
 const UserSchema = z.object({
-    id: z.coerce.number().int(),
+    id: z.string(),
     name: z.string().min(1),
     email: z.string().email(),
-    password: z.string().min(8),
+    image: z.string().or(z.null()).or(z.undefined()),
+    emailVerified: z.boolean(),
     createdAt: z.coerce.date(),
     updatedAt: z.coerce.date(),
 })
 
-const UserSafeSchema = UserSchema.omit({ password: true })
-const UserSignInSchema = UserSchema.pick({ email: true, password: true })
-const UserSignUpSchema = UserSchema.pick({ name: true, email: true, password: true })
-const UserForgotPasswordSchema = UserSchema.pick({ email: true })
-
 //
 
 type UserSchema = z.infer<typeof UserSchema>
-type UserSafeSchema = z.infer<typeof UserSafeSchema>
-type UserSignInSchema = z.infer<typeof UserSignInSchema>
-type UserSignUpSchema = z.infer<typeof UserSignUpSchema>
-type UserForgotPasswordSchema = z.infer<typeof UserForgotPasswordSchema>
 
 //
 
-export { UserSchema, UserSafeSchema, UserSignInSchema, UserSignUpSchema, UserForgotPasswordSchema }
+export { UserSchema }
