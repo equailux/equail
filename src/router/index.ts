6ff9ffc7fae9d0d840d1f675bd65612cd2c31cbc
equailux <equail2026@gmail.com>
2026-04-24 09:15:17 +0800

    Implemented auth middleware.

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,7 @@ import SensorView from "@/views/app/config/SensorView.vue"
 import ThresholdView from "@/views/app/config/ThresholdView.vue"
 import ConditionView from "@/views/app/config/ConditionView.vue"
 import ActionView from "@/views/app/config/ActionView.vue"
+import { redirectAuth, refreshAuth, requireAuth } from "@/middlewares/auth.middleware"
 
 //
 
@@ -33,96 +34,112 @@ const routes: RouteRecordRaw[] = [
         name: "Sign Up",
         meta: { layout: "auth" },
         component: SignUpView,
+        beforeEnter: [refreshAuth, redirectAuth],
     },
     {
         path: "/auth/sign-in",
         name: "Sign In",
         meta: { layout: "auth" },
         component: SignInView,
+        beforeEnter: [refreshAuth, redirectAuth],
     },
     {
         path: "/auth/forgot-password",
         name: "Forgot Password",
         meta: { layout: "auth" },
         component: ForgotPasswordView,
+        beforeEnter: [refreshAuth, redirectAuth],
     },
     {
         path: "/auth/reset-password",
         name: "Reset Password",
         meta: { layout: "auth" },
         component: ResetPasswordView,
+        beforeEnter: [refreshAuth, redirectAuth],
     },
     {
         path: "/app/dashboard",
         name: "Dashboard",
         meta: { layout: "app" },
         component: DashboardView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/dashboard/detection",
         name: "Detection",
         meta: { layout: "dashboard" },
         component: DetectionView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/dashboard/detection/:cid(\\d+)",
         name: "Detection Preview",
         meta: { layout: "detection" },
         component: DetectionPreviewView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/dashboard/mortality",
         name: "Mortality",
         meta: { layout: "dashboard" },
         component: MortalityView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/controls",
         name: "Controls",
         meta: { layout: "app" },
         component: ControlsView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/settings",
         name: "Settings",
         meta: { layout: "app" },
         component: SettingsView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/analytics",
         name: "Analytics",
         meta: { layout: "app" },
         component: AnalyticsView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/config/actuator",
         name: "Config Actuator",
         meta: { layout: "app/config" },
         component: ActuatorView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/config/sensor",
         name: "Config Sensor",
         meta: { layout: "app/config" },
         component: SensorView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/config/threshold",
         name: "Config Threshold",
         meta: { layout: "app/config" },
         component: ThresholdView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/config/condition",
         name: "Config Condition",
         meta: { layout: "app/config" },
         component: ConditionView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/config/action",
         name: "Config Action",
         meta: { layout: "app/config" },
         component: ActionView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: '/:pathMatch(.*)*',
