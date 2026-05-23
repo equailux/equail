4f49d203a32634d2b6677f33c8034b784ea4fa70
equailux <equail2026@gmail.com>
2026-05-23 13:56:14 +0800

    Created feed schema.

---
+++ src/schemas/FeedSchema.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+
+//
+
+const FeedSchema = z.object({
+	id: z.coerce.number().int(),
+	level: z.coerce.number(),
+	detectionId: z.coerce.number().int().nullable(),
+	createdAt: z.coerce.date(),
+	updatedAt: z.coerce.date(),
+})
+
+const FeedQuerySchema = FeedSchema.partial()
+const FeedCreateSchema = FeedSchema.pick({ level: true, detectionId: true })
+const FeedUpdateSchema = FeedSchema.pick({ level: true, detectionId: true }).partial()
+
+//
+
+type FeedSchema = z.infer<typeof FeedSchema>
+type FeedQuerySchema = z.infer<typeof FeedQuerySchema>
+type FeedCreateSchema = z.infer<typeof FeedCreateSchema>
+type FeedUpdateSchema = z.infer<typeof FeedUpdateSchema>
+
+//
+
+export { FeedSchema, FeedQuerySchema, FeedCreateSchema, FeedUpdateSchema }
