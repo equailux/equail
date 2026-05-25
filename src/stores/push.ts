5ae365539cb6c57222550c1635656439548c3d68
equailux <equail2026@gmail.com>
2026-05-25 16:45:08 +0800

    Created push store.

---
+++ src/stores/push.ts
@@ -0,0 +1,38 @@
+import { api } from "@/plugins/api"
+import { Capacitor } from "@capacitor/core"
+import { PushNotifications, type Token } from "@capacitor/push-notifications"
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+//
+
+export const usePushStore = defineStore("push", () => {
+
+    //
+
+    const connected = ref(false)
+
+    //
+
+    const connect = async () => {
+        const isNative = Capacitor.isNativePlatform()
+        if (!isNative || connected.value) return
+
+        const perm = await PushNotifications.requestPermissions()
+        if (perm.receive !== "granted") return console.info(`Push notification permission denied.`)
+
+        await PushNotifications.register()
+        PushNotifications.addListener("registration", register)
+    }
+
+    const register = async (token: Token) => {
+        await api
+            .post("/api/device", { token: token.value })
+            .then(() => connected.value = true)
+            .catch(() => connected.value = false)
+    }
+
+    //
+
+    return { connected, connect, register }
+})
